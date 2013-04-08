1cae3ecb851fba556f21988f901c487286af4051
Samvel Stepanyan <samvelst@gmail.com>
2013-04-07 23:35:29 -0700

    added dynamic titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "StaticPages" do
             visit '/static_pages/home'
             expect(page).to have_content('Sample App')
         end
+
+        it "should have the right title" do
+            visit '/static_pages/home'
+            expect(page).to have_title('Sample App | Home')
+        end
     end
 
     describe "Help page" do
@@ -15,12 +20,22 @@ describe "StaticPages" do
             visit '/static_pages/help'
             expect(page).to have_content('Help')
         end
+
+        it "should have the right title" do
+            visit '/static_pages/help'
+            expect(page).to have_title('Sample App | Help')
+        end
     end
 
     describe "About page" do
         it "should have the content 'About' " do
             visit '/static_pages/about'
             expect(page).to have_content('About')
+        end
+
+        it "should have the right title" do
+            visit '/static_pages/about'
+            expect(page).to have_title('Sample App | About')
         end
     end
 
