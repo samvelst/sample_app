98a5d6cfa9b7cfa92cf1189f2f7e40b5a66c884d
Samvel Stepanyan <samvelst@gmail.com>
2013-04-08 00:29:02 -0700

    added static shit and fixed tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-    describe "Home page" do
+    let(:base_title) { 'Sample App' }
 
+    describe "Home page" do
         it "should have the content 'Sample App' " do
             visit '/static_pages/home'
             expect(page).to have_content('Sample App')
@@ -11,7 +12,7 @@ describe "StaticPages" do
 
         it "should have the right title" do
             visit '/static_pages/home'
-            expect(page).to have_title('Sample App | Home')
+            expect(page).to have_title("#{base_title} | Home")
         end
     end
 
@@ -23,7 +24,7 @@ describe "StaticPages" do
 
         it "should have the right title" do
             visit '/static_pages/help'
-            expect(page).to have_title('Sample App | Help')
+            expect(page).to have_title("#{base_title} | Help")
         end
     end
 
@@ -35,8 +36,19 @@ describe "StaticPages" do
 
         it "should have the right title" do
             visit '/static_pages/about'
-            expect(page).to have_title('Sample App | About')
+            expect(page).to have_title("#{base_title} | About")
         end
     end
 
+    describe "Contact page" do
+        it "should have the content 'Contact' " do
+            visit '/static_pages/contact'
+            expect(page).to have_content('Contact')
+        end
+
+        it "should have the right title" do
+            visit '/static_pages/contact'
+            expect(page).to have_title('Sample App | Contact')
+        end
+    end
 end
